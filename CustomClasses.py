bb9e25073c914ac39f217b14f3c84a9cc38c6995
Lenyeto <patg.lenyeto@gmail.com>
2015-02-08 12:29:44 -0800

    Misc Changes

--- CustomClasses.py
+++ CustomClasses.py
@@ -1,22 +1,24 @@
 
 class EntityBase(object):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
 
 
 class ItemBase(object):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
 
 
 class Player(EntityBase):
-    def __init__(self):
-        pass
+    def __init__(self, health):
+        self.name = "Hero"
+        self.health = health
 
 
 class Enemy(EntityBase):
-    def __init__(self):
-        pass
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
 
 
 class Consumable(ItemBase):
