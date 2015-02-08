7b872f88c690dbff1ec4046901045f9860d65645
Lenyeto <patg.lenyeto@gmail.com>
2015-02-08 10:47:48 -0800

    Removed the Test file, Added some Misc Stuff

---
+++ CustomClasses.py
@@ -0,0 +1,29 @@
+
+class EntityBase(object):
+    def __init__(self):
+        pass
+
+
+class ItemBase(object):
+    def __init__(self):
+        pass
+
+
+class Player(EntityBase):
+    def __init__(self):
+        pass
+
+
+class Enemy(EntityBase):
+    def __init__(self):
+        pass
+
+
+class Consumable(ItemBase):
+    def __init__(self):
+        pass
+
+
+class Equipable(ItemBase):
+    def __init__(self):
+        pass
---
+++ Game.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+resolution = (600, 800)
+
+window = pygame.display.set_mode(resolution)
+clock = pygame.time.Clock()
+
+done = False
+while not done:
+    dtime = clock.tick()
+
+    evtList = pygame.event.get()
+    for evt in evtList:
+        if evt.type == pygame.QUIT:
+            done = True
+
+    window.fill((0, 0, 0))
+
+    pygame.display.flip()
+pygame.quit()
