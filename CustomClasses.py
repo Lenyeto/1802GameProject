ab60c24f7e9ed26cdeeaec1d2ec40aff399d4238
Lenyeto <patg.lenyeto@gmail.com>
2015-04-01 13:58:48 -0400

    Revert "worked on movement and combat"
    This reverts commit 4dc793946b841ce95017f372202f429c68117114.

---
+++ CustomClasses.py
@@ -0,0 +1,116 @@
+import mymath
+import xml.etree.ElementTree as ET
+
+ITEM_CONSUMABLE = 0
+ITEM_EQUIPABLE = 1
+
+EQUIP_HELM = 0
+EQUIP_CHEST = 1
+EQUIP_LEGS = 2
+EQUIP_FOOT = 3
+EQUIP_WEAPON = 4
+EQUIP_OFFHAND = 5
+EQUIP_TRINKET = 6
+
+NONE = 0
+CON_HEALTH = 1
+CON_POWER = 2
+
+class EntityBase(object):
+    def __init__(self, name, pos):
+        self.name = name
+        self.pos = pos
+
+    def ai_player(self, player):
+        pass
+
+
+class ItemBase(object):
+    def __init__(self, name, file):
+        self.name = name
+        self.stats = self.get_data(self, file)
+
+    def use(self, player):
+        pass
+
+    def get_data(self, file):
+        tree = ET.parse(file)
+        root = tree.getroot()
+        return True #PLACE HOLDER
+
+
+
+class Player(EntityBase):
+    def __init__(self, health):
+        self.name = "Hero"
+        self.health = health
+        self.equipment = {'Helmet', 'Chest', 'Legs', 'Foot', 'MainHand', 'OffHand', 'Consumable'}
+        self.equipment['Trinkets'] = []
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+
+
+
+class Enemy(EntityBase):
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+
+class Consumable(ItemBase):
+    def __init__(self):
+        pass
+
+
+class Equipable(ItemBase):
+    def __init__(self, name, type, file):
+        ItemBase.__init__(name, file)
+        self.type = type
+
+
+
+class ItemStand(EntityBase):
+    def __init__(self, item, pos, param):
+        EntityBase.__init__(self, "", pos)
+        if isinstance(item, Consumable):
+            self.type = ITEM_CONSUMABLE
+        elif isinstance(item, Equipable):
+            self.type = ITEM_EQUIPABLE
+        else:
+            raise TypeError("This can only hold items.")
+
+        if self.type == ITEM_CONSUMABLE:
+            self.potable = param
+        else:
+            self.equipable = param[1]
+            self.item = Equipable(param[0], param[1], param[3])
+
+        self.cool_down = 10
+
+    def pick_up(self, player):
+        if self.type == ITEM_CONSUMABLE:
+            if player.equipment['Consumable'] == NONE:
+                player.equipment['Consumable'] = self.potable
+                self.potable = NONE
+            else:
+                tmp = player.equipment['Consumable']
+                player.equipment['Consumable'] = self.potable
+                self.potable = tmp
+        self.cool_down = 10
+
+    def update(self, dt, players=[]):
+        if self.cool_down == 0:
+            for p in players:
+                dist_vect = p.pos - self.pos
+                dist = dist_vect.length()
+                if dist < 5:
+                    self.pick_up(p)
+        else:
+            self.cool_down -= dt
+            if self.cool_down < 0:
+                self.cool_down = 0
