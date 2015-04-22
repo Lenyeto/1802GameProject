f208914241efa6565fc1456f21168fcd6413cb0a
Lenyeto <patg.lenyeto@gmail.com>
2015-04-22 15:01:47 -0400

    Updated Some Stuff

--- EntitiyClasses.py
+++ EntitiyClasses.py
@@ -3,6 +3,8 @@ import xml.etree.ElementTree as ET
 import pygame
 import math
 import random
+from xmlweapons import *
+from UtilFuncs import *
 
 ITEM_CONSUMABLE = 0
 ITEM_EQUIPABLE = 1
@@ -27,34 +29,49 @@ LEFT = mymath.Vector2(-1, 0)
 
 
 class EntityBase(object):
-    def __init__(self, name, pos, velocity=mymath.Vector2(0, 0)):
+    def __init__(self, name, pos, velocity=mymath.Vector2(0, 0), physical=True, background=False):
         self.name = name
         self.pos = pos
         self.velocity = velocity
+        self.physical = physical
+        self.background = background
 
     def ai_player(self, player):
         pass
 
+    def hit(self, x, k=mymath.Vector2(0, 0)):
+        pass
+
+    def update(self, dt):
+        pass
+
 class Projectile(EntityBase):
-    def __init__(self, pos, velocity, height=1):
-        self.pos = pos
+    def __init__(self, pos, velocity, height=1, damage=1):
+        self.pos = pos.copy()
         self.velocity = velocity
         self.height = height
         self.isPiercing = False
-        self.attack = 5
+        self.attack = damage
 
     def update(self, dt, entityList):
         self.pos += self.velocity * dt
         for e in entityList:
-            tmp = self.pos - e.pos
-            dist = tmp.Dot(tmp)
-            if dist < 16**2:
-                self.damage(e)
-                return True
+            if isinstance(e, Wall_Floor):
+                if e.physical:
+                    tmp = self.pos - e.pos
+                    dist = tmp.Dot(tmp)
+                    if dist < 16 ** 2:
+                        return True
+            else:
+                tmp = self.pos - e.pos
+                dist = tmp.Dot(tmp)
+                if dist < 16**2:
+                    self.damage(e)
+                    return True
         return False
 
     def damage(self, other):
-        other.health -= self.attack
+        other.hit(self.attack)
 
     def render(self, surface):
         pygame.draw.circle(surface, (255, 0, 0), (int(self.pos.x), int(self.pos.y)), 5)
@@ -117,6 +134,29 @@ class TrackingProj(Projectile):
     def render(self, surface):
         Projectile.render(self, surface)
 
+class particle(object):
+    def __init__(self, pos, image, life, direction):
+        self.pos = pos
+        self.image = image
+        self.life = life
+        if direction == UP:
+            self.rotation = 0
+        elif direction == RIGHT:
+            self.rotation = 90
+        elif direction == LEFT:
+            self.rotation = 180
+        elif direction == DOWN:
+            self.rotation = 270
+
+    def update(self, dt):
+        self.life -= dt
+        if self.life < 0:
+            return True
+        return False
+
+    def render(self, surface):
+        tmp = pygame.transform.rotate(self.image, self.rotation)
+        #surface.blit(tmp, (self.pos.x - tmp.get_width()/2, self.pos.y - tmp.get_height()/2))
 
 
 class Player(EntityBase):
@@ -125,30 +165,54 @@ class Player(EntityBase):
         self.name = "Hero"
         self.health = health
         self.speed = 0.4
+
+
         self.equipment = [-1, -1, -1, -1, -1, -1, [], []]
-        #self.equipment = {'Helmet', 'Chest', 'Legs', 'Foot', 'MainHand', 'OffHand', 'Consumable', 'Trinkets'}
-        #self.equipment['Trinkets'] = []
         self.direction = DOWN
         self.invincibility_period = 0
         self.attack_delay = 1000
         self.cur_delay = 0
         self.sub_entities = []
+        self.particles = []
+
 
         self.tmp_key_list = []
 
-        if debug:
-            self.equipment[EQUIP_WEAPON] = "Bow"
+        #if debug:
+        self.equipment[EQUIP_WEAPON] = Weapon()
+        self.equipment[EQUIP_WEAPON].getWeapon("Longsword")
 
 
     def update(self, dt, list_of_keys, list_of_entities):
+        #equip_weight_sum = 0
+        #for i in self.equipment:
+        #    if not i == -1 and not isinstance(i, list):
+        #        equip_weight_sum += i.weight / 10
+        #self.speed = 0.4 - equip_weight_sum
+        #if self.speed < 0.05:
+        #    self.speed = 0.05
+
+
+
+
+
         if self.cur_delay > 0:
             self.cur_delay -= dt
         if self.cur_delay < 0:
             self.cur_delay = 0
 
+        if self.invincibility_period > 0:
+            self.invincibility_period -= dt
+        if self.invincibility_period < 0:
+            self.invincibility_period = 0
+
         for i in self.sub_entities:
             if i.update(dt, list_of_entities):
                 self.sub_entities.remove(i)
+
+        for i in self.particles:
+            if i.update(dt):
+                self.particles.remove(i)
 
         if pygame.K_UP in list_of_keys:
             self.direction = UP
@@ -172,34 +236,63 @@ class Player(EntityBase):
                 self.attack(list_of_entities)
 
         if pygame.K_w in list_of_keys:
-            self.pos += UP * dt * self.speed
+            self.attemptMove(UP, list_of_entities, dt)
         if pygame.K_s in list_of_keys:
-            self.pos += DOWN * dt * self.speed
+            self.attemptMove(DOWN, list_of_entities, dt)
         if pygame.K_a in list_of_keys:
-            self.pos += LEFT * dt * self.speed
+            self.attemptMove(LEFT, list_of_entities, dt)
         if pygame.K_d in list_of_keys:
-            self.pos += RIGHT * dt * self.speed
+            self.attemptMove(RIGHT, list_of_entities, dt)
+
+    def attemptMove(self, direction, list_of_entities, dt):
+        new_position = self.pos + direction
+        for e in list_of_entities:
+            if e.physical:
+                if isinstance(e, Wall_Floor):
+                    tmp = new_position - e.pos
+                    newDist = tmp.Dot(tmp)
+                    tmp2 = self.pos - e.pos
+                    oldDist = tmp2.Dot(tmp2)
+                    if newDist < 20**2 and newDist < oldDist:
+                        return False
+                else:
+                    tmp = new_position - e.pos
+                    newDist = tmp.Dot(tmp)
+                    tmp2 = self.pos - e.pos
+                    oldDist = tmp2.Dot(tmp2)
+
+                    if newDist < 40**2 and newDist < oldDist:
+                        return False
+        self.pos += direction * dt * self.speed
+        return True
 
 
     def render(self, surface):
         pygame.draw.circle(surface, (255, 0, 0), (int(self.pos.x), int(self.pos.y)), 20)
         for i in self.sub_entities:
             i.render(surface)
+        for i in self.particles:
+            i.render(surface)
 
 
     def attack(self, list_of_entities):
-        self.cur_delay = self.attack_delay
-        if self.equipment[EQUIP_WEAPON] == "Sword":
-            for i in list_of_entities:
-                tmp = i.pos - self.pos
-                tmp = tmp.Dot(tmp)
-                if tmp < 64 ** 2:
-                    i.hit(5, self.direction * 20)
-        elif self.equipment[EQUIP_WEAPON] == "Bow":
-            #self.sub_entities.append(TestProj(self.pos.copy(), self.direction.copy()))
-            pass
-        elif self.equipment[EQUIP_WEAPON] == "Spell":
-            pass
+        if isinstance(self.equipment[EQUIP_WEAPON], Weapon):
+            self.cur_delay = self.attack_delay
+            if self.equipment[EQUIP_WEAPON].weapon['wtype'] == 'Melee':
+                for i in list_of_entities:
+                    tmp = i.pos - self.pos
+                    tmp = tmp.Dot(tmp)
+                    if tmp < int(self.equipment[EQUIP_WEAPON].weapon['range'])** 2:
+                        i.hit(self.equipment[EQUIP_WEAPON].weapon['damage'], self.direction * 20)
+                self.particles.append(particle(self.pos + self.direction * 10, swipe, 500, self.direction))
+            elif self.equipment[EQUIP_WEAPON].weapon['wtype'] == "Ranged":
+                self.sub_entities.append(Projectile(self.pos, self.direction*self.equipment[EQUIP_WEAPON].weapon['velocity'], 1, self.equipment[EQUIP_WEAPON].weapon['damage']))
+            elif self.equipment[EQUIP_WEAPON].weapon['wtype'] == "Spell":
+                pass
+
+    def hit(self, damage):
+        self.health -= damage
+        self.invincibility_period = 5
 
 
 
@@ -210,58 +303,79 @@ class Enemy(EntityBase):
 
 
 class ItemStand(EntityBase):
-    def __init__(self, item, pos, param):
+    def __init__(self, item, pos):
         EntityBase.__init__(self, "", pos)
-        if isinstance(item, Consumable):
-            self.type = ITEM_CONSUMABLE
-        elif isinstance(item, Equipable):
-            self.type = ITEM_EQUIPABLE
-        else:
-            raise TypeError("This can only hold items.")
 
-        if self.type == ITEM_CONSUMABLE:
-            self.potable = param
-        else:
-            self.equipable = param[1]
-            self.item = Equipable(param[0], param[1], param[3])
+        self.item = item
 
         self.cool_down = 10
 
     def pick_up(self, player):
-        if self.type == ITEM_CONSUMABLE:
-            if player.equipment['Consumable'] == NONE:
-                player.equipment['Consumable'] = self.potable
-                self.potable = NONE
-            else:
-                tmp = player.equipment['Consumable']
-                player.equipment['Consumable'] = self.potable
-                self.potable = tmp
-        self.cool_down = 10
+
+        self.cool_down = 1000
 
     def update(self, dt, players=[]):
         if self.cool_down == 0:
             for p in players:
                 dist_vect = p.pos - self.pos
-                dist = dist_vect.length()
-                if dist < 5:
+                dist = dist_vect.Dot(dist_vect)
+                if dist < 50**2:
                     self.pick_up(p)
         else:
             self.cool_down -= dt
             if self.cool_down < 0:
                 self.cool_down = 0
 
+    def render(self, surface):
+        pygame.draw.rect(surface, (125, 125, 125), (int(self.pos.x-20), int(self.pos.y+20), 40, 20))
+        if not self.item == -1:
+            pygame.draw.circle(surface, (self.cool_down/1000 * 255, 225, 225), (int(self.pos.x), int(self.pos.y)), 15)
+
+
+class WeaponStand(ItemStand):
+    def __init__(self, weapon, pos):
+        ItemStand.__init__(self, weapon, pos)
+
+    def pick_up(self, player):
+        if isinstance(player.equipment[EQUIP_WEAPON], Weapon):
+            if not self.item == -1:
+                tmp = self.item
+                self.item = player.equipment[EQUIP_WEAPON]
+                player.equipment[EQUIP_WEAPON] = tmp
+        else:
+            player.equipment[EQUIP_WEAPON] = self.item
+            self.item = -1
+        self.cool_down = 1000
+
+    def render(self, surface):
+        pygame.draw.rect(surface, (self.cool_down/1000 * 255, 225, 225), (int(self.pos.x-20), int(self.pos.y + 20), 40, 20))
+        tmpImage = pygame.image.load(self.item.weapon['graphic'])
+        surface.blit(tmpImage, (int(self.pos.x - tmpImage.get_width()/2), int(self.pos.y - tmpImage.get_height()/2)))
+
+
 
 class Dummy(EntityBase):
     def __init__(self, pos):
-        EntityBase.__init__(self, "DUMMY", pos)
+        EntityBase.__init__(self, "ENEMY", pos)
         self.health = 100
         self.invincible = 0
         self.show_health = 0
+        self.speed = 0.01
+        self.target = None
 
     def hit(self, damage, knockback=mymath.Vector2(0, 0)):
         self.health -= damage
         self.pos += knockback
         self.show_health = 10
+
+    def attemptMove(self, direction, players, dt):
+        new_position = self.pos + direction
+        for e in players:
+            tmp = new_position - e.pos
+            if tmp.Dot(tmp) < 40**2:
+                return False
+        self.pos += direction * dt * self.speed
+        return True
 
     def update(self, dt):
         if self.invincible > 0:
@@ -284,3 +398,29 @@ class Dummy(EntityBase):
         tmpX = 40
         tmpX *= self.health / 100
         pygame.draw.rect(surface, (125, 125, 125), (int(self.pos.x) - tmpX/2, int(self.pos.y - 10) - 20, tmpX, 5))
+
+    def AI(self, players, dt):
+        if self.target == None:
+            if len(players) > 1:
+                distList = []
+                distList2 = players.copy()
+                for p in players:
+                    tmp = self.pos - p.pos
+                    dist = tmp.Dot(tmp)
+                    distList.append(dist)
+                distList.sort()
+                distList2.index(distList[0])
+            else:
+                self.target = players[0]
+        else:
+            dir = (self.target.pos - self.pos).getNormalized()
+            self.attemptMove(dir, players, dt)
+
+
+class Wall_Floor(EntityBase):
+    def __init__(self, name, pos, texture, physical=True, background=True):
+        EntityBase.__init__(self, name, pos, mymath.Vector2(0, 0), physical, background)
+        self.texture = pygame.transform.scale(pygame.image.load(texture), (32, 32))
+
+    def render(self, surface):
+        surface.blit(self.texture, (self.pos.x, self.pos.y))
--- MapSystem.py
+++ MapSystem.py
@@ -1,1 +1,186 @@
-__author__ = 'buellw'
+import random
+import pygame
+from mymath import *
+from EntitiyClasses import *
+import xmlweapons
+
+
+
+DEBUG = -1
+
+
+
+
+class Floor(object):
+    def __init__(self, surface, type=DEBUG):
+        self.rooms = []
+        self.type = type
+        self.num_rooms = random.randint(6, 16)
+        self.generate_floor()
+        self.cur_room = self.rooms[0]
+        self.create_doors()
+        self.cur_room.generate_room(surface)
+
+
+
+    def generate_floor(self):
+        self.rooms.append(Room(self.type, Vector2(0, 0)))
+        #for i in range(self.num_rooms):
+        i = 0
+        while i < self.num_rooms:
+            cur_room = self.rooms[-1]
+            rnd = random.randint(0, 3)
+            if len(self.rooms) <= self.num_rooms:# - 3:
+                print(len(self.rooms))
+                if rnd == 0:
+                    if self.check_if_full(Vector2(cur_room.position.x + 1, cur_room.position.y)) == False:
+                        self.rooms.append(Room(self.type, Vector2(cur_room.position.x + 1, cur_room.position.y)))
+                        i += 1
+                elif rnd == 1:
+                    if not self.check_if_full(Vector2(cur_room.position.x, cur_room.position.y + 1)):
+                        self.rooms.append(Room(self.type, Vector2(cur_room.position.x, cur_room.position.y + 1)))
+                        i += 1
+                elif rnd == 2:
+                    if not self.check_if_full(Vector2(cur_room.position.x - 1, cur_room.position.y)):
+                        self.rooms.append(Room(self.type, Vector2(cur_room.position.x - 1, cur_room.position.y)))
+                        i += 1
+                else:
+                    if not self.check_if_full(Vector2(cur_room.position.x, cur_room.position.y - 1)):
+                        self.rooms.append(Room(self.type, Vector2(cur_room.position.x, cur_room.position.y - 1)))
+                        i += 1
+            #elif len(self.rooms) < self.num_rooms - 1:
+            #    # Creates Item Rooms
+            #    pass
+            #elif len(self.rooms) < self.num_rooms:
+            #    # Creates Boss Room
+            #    pass
+
+    def update(self, list_of_players, surface):
+        move = self.cur_room.update(list_of_players)
+        if not move == Vector2(0, 0):
+            bool = self.check_if_full(self.cur_room.position + move)
+            if bool:
+                self.cur_room.entities = list()
+                self.cur_room = self.get_room_at(self.cur_room.position + move)
+                self.cur_room.generate_room(surface)
+                for i in list_of_players:
+                    if move == Vector2(-1, 0):
+                        i.pos = Vector2(550, 320)
+                    elif move == Vector2(0, 1):
+                        i.pos = Vector2(320, 550)
+                    elif move == Vector2(1, 0):
+                        i.pos = Vector2(90, 320)
+                    elif move == Vector2(0, -1):
+                        i.pos = Vector2(320, 90)
+
+    def create_doors(self):
+        for i in self.rooms:
+            if self.check_if_full(Vector2(i.position.x + 1, i.position.y)):
+                i.doors[0] = True
+            if self.check_if_full(Vector2(i.position.x, i.position.y + 1)):
+                i.doors[1] = True
+            if self.check_if_full(Vector2(i.position.x - 1, i.position.y)):
+                i.doors[2] = True
+            if self.check_if_full(Vector2(i.position.x, i.position.y - 1)):
+                i.doors[3] = True
+
+    def check_if_full(self, pos):
+        for i in self.rooms:
+            if i.position == pos:
+                return True
+        return False
+
+    def get_room_at(self, pos=Vector2(0, 0)):
+        for i in self.rooms:
+            if i.position == pos:
+                return i
+
+
+class Room(object):
+    def __init__(self, type, position=Vector2(0, 0)):
+        self.entities = []
+        self.can_leave = False
+        self.position = position
+        self.doors = [False, False, False, False]
+        self.door_pos = [Vector2(0, 0), Vector2(0, 0), Vector2(0,0), Vector2(0, 0)]
+        self.empty = False
+
+    def generate_room(self, surface, type=DEBUG):
+        self.create_walls(surface, type)
+        self.generate_entities(surface, type)
+
+    def regenerate_room(self, surface, type=DEBUG):
+        self.create_walls(surface, type)
+
+
+    def create_walls(self, surface, type=DEBUG):
+        for i in range(int(surface.get_width()/32)):
+            for k in range(int(surface.get_height()/32)):
+                if type == DEBUG:
+                    path = "Assets/Graphics/Room/"
+                if i == 0 and k == 0:
+                    self.entities.append(Wall_Floor("Wall", Vector2(0, 0), path+"NorthWestWall.png"))
+                elif i == surface.get_width()/32 - 1 and k == surface.get_height()/32 - 1:
+                    self.entities.append(Wall_Floor("Wall", Vector2(surface.get_width() - 32, surface.get_height() - 32), path+"SouthEastWall.png"))
+                elif 0 < i < surface.get_width()/32 - 1 and 0 < k < surface.get_height()/32 - 1:
+                    self.entities.append(Wall_Floor("Floor", Vector2(i * 32, k * 32), path+"StoneFloor1.png", False))
+                elif k == surface.get_height()/32 - 1 and i == 0:
+                    self.entities.append(Wall_Floor("Wall", Vector2(i * 32, k * 32), path+"SouthWestWall.png"))
+                elif i == surface.get_width()/32 - 1 and k == 0:
+                    self.entities.append(Wall_Floor("Wall", Vector2(i * 32, k * 32), path+"NorthEastWall.png"))
+                else:
+                    if i == 0 and not k == 0 and not k == surface.get_height()/32 - 1:
+                        self.entities.append(Wall_Floor("Wall", Vector2(i * 32, k * 32), path+"WestWall.png"))
+                    if not i == 0 and k == 0 and not i == surface.get_width()/32 - 1:
+                        self.entities.append(Wall_Floor("Wall", Vector2(i * 32, k * 32), path+"NorthWall.png"))
+                    if i == surface.get_width()/32 -1 and not k == 0 and not k == surface.get_height()/32 -1:
+                        self.entities.append(Wall_Floor("Wall", Vector2(i * 32, k * 32), path+"EastWall.png"))
+                    if k == surface.get_height()/32 -1 and not i == 0 and not i == surface.get_width()/32 - 1:
+                        self.entities.append(Wall_Floor("Wall", Vector2(i * 32, k * 32), path+"SouthWall.png"))
+        if self.doors[0]:
+            self.entities.append(Wall_Floor("Door", Vector2(surface.get_width() - 32, surface.get_height()/2 - 16), path + "EastDoor.png"))
+            self.door_pos[0] = Vector2(surface.get_width() - 16, surface.get_height()/2 - 16)
+        if self.doors[1]:
+            self.entities.append(Wall_Floor("Door", Vector2(surface.get_width()/2 - 16, 0), path + "NorthDoor.png"))
+            self.door_pos[1] = Vector2(surface.get_width()/2 - 16, 16)
+        if self.doors[2]:
+            self.entities.append(Wall_Floor("Door", Vector2(0, surface.get_height()/2 - 16), path + "WestDoor.png"))
+            self.door_pos[2] = Vector2(16, surface.get_height()/2 - 16)
+        if self.doors[3]:
+            self.entities.append(Wall_Floor("Door", Vector2(surface.get_width()/2 - 16, surface.get_height() - 32), path + "SouthDoor.png"))
+            self.door_pos[3] = Vector2(surface.get_width()/2 - 16, surface.get_height() - 16)
+
+    def generate_entities(self, surface, type):
+        self.entities.append(Dummy(mymath.Vector2(200, 200)))
+        self.entities.append(WeaponStand(xmlweapons.Weapon().getWeapon("Short Bow"), mymath.Vector2(50, 50)))
+
+    def render(self, surface):
+        for e in self.entities:
+            e.render(surface)
+
+    def update(self, list_of_players):
+        if not self.can_leave:
+            num_of_enemies = 0
+            for i in self.entities:
+                if i.name == "ENEMY":
+                    num_of_enemies += 1
+            if num_of_enemies > 0:
+                self.can_leave = False
+            else:
+                self.can_leave = True
+        else:
+            for i in list_of_players:
+                for j in range(len(self.door_pos)):
+                    tmp = i.pos - self.door_pos[j]
+                    dist = tmp.Dot(tmp)
+                    if self.doors[j] and dist < 48 ** 2:
+                        if j == 2:
+                            return Vector2(-1, 0)
+                        elif j == 0:
+                            return Vector2(1, 0)
+                        elif j == 1:
+                            return Vector2(0, 1)
+                        elif j == 3:
+                            return Vector2(0, -1)
+            return Vector2(0, 0)
+        return Vector2(0, 0)
